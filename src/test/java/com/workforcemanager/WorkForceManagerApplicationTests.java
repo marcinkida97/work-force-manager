c85291d3632907e7102aa5686135aca6b927ee00
Marcin Kida <73959245+marcinkida97@users.noreply.github.com>
2024-02-19 18:52:37 +0100

    Project init

---
+++ src/test/java/com/workforcemanager/WorkForceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workforcemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkForceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
